1585def050066fa6e2e5eeb21c675ab85b67475c
Vardhaman Bhandari <sbhandari78504@gmail.com>
2023-11-14 13:16:48 +0530

    added icons

---
+++ src/components/icons/custom/close.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+import { IconProps } from "../types";
+
+export default function CloseIcon({ className, ...props }: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={cn("h-5 w-5", className)}
+      {...props}
+    >
+      <path d="M18 6 6 18" />
+      <path d="m6 6 12 12" />
+    </svg>
+  );
+}
