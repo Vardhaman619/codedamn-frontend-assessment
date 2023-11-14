4735635dcfcebe6f614ef32609106f90194c3b93
Vardhaman Bhandari <sbhandari78504@gmail.com>
2023-11-14 13:34:57 +0530

    added icons

---
+++ src/components/icons/custom/education.tsx
@@ -0,0 +1,20 @@
+import { cn } from "~/lib/utils";
+import type { IconProps } from "../types";
+export default function EducationIcon({ className, ...props }: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={cn("h-5 w-5", className)}
+      {...props}
+    >
+      <path d="M22 10v6M2 10l10-5 10 5-10 5z" />
+      <path d="M6 12v5c3 3 9 3 12 0v-5" />
+    </svg>
+  );
+}
---
+++ src/components/icons/custom/portfolio.tsx
@@ -0,0 +1,21 @@
+import { cn } from "~/lib/utils";
+import type { IconProps } from "../types";
+export default function PortfolioIcon({ className, ...props }: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={cn("h-5 w-5", className)}
+      {...props}
+    >
+      <path d="M3 2h18" />
+      <rect width="18" height="12" x="3" y="6" rx="2" />
+      <path d="M3 22h18" />
+    </svg>
+  );
+}
