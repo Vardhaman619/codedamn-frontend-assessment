af820e390a3aa43a9b068a606ff836334889ac8c
Vardhaman Bhandari <sbhandari78504@gmail.com>
2023-12-06 17:02:56 +0530

    updated prisma schema and deleted lib/uploadthing

--- src/lib/uploadthing.ts
+++
@@ -1,8 +0,0 @@
-// import { generateComponents } from "@uploadthing/react";
-import { generateReactHelpers } from "@uploadthing/react/hooks";
-
-import type { UploadThingRouter } from "~/app/api/uploadthing/core";
-
-// export const { UploadButton, UploadDropzone, Uploader } =
-//   generateComponents<UploadThingRouter>();
-export const { useUploadThing } = generateReactHelpers<UploadThingRouter>();
